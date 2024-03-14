57378845f43eb1ecf144a28f03940b177dae86c7
coolCicada <1475395755@qq.com>
2024-03-14 23:52:41 +0800

    feat: ABC 344 A

---
+++ ABC/344/A.js
@@ -0,0 +1,30 @@
+function main() {
+    const S = cinS();
+    const arr = S.split('|')
+    cout(arr[0] + arr[2])
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '', content = null, cid = 0;
+
+process.stdin.on('data', (inputStdin) => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', (_) => {
+    content=inputString.split(/ |\r\n|\n/), cid=0;
+    main();
+})
+
+function cin() {
+    return Number(content[cid++])
+}
+function cinS() {
+    return content[cid++]
+}
+
+function cout(...args) {
+    console.log(...args);
+}
