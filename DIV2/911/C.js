c1c604f4005faf187b0f83cd4aaca90e5233e766
coolCicada <1475395755@qq.com>
2024-03-23 22:13:54 +0800

    feat: DIV2/911 迭代写法

--- DIV2/911/C.js
+++ DIV2/911/C.js
@@ -11,22 +11,35 @@ function solve() {
         LR[i] = [cin(), cin()]
     }
 
-    function dfs(i) {
+    const dp = new Array(n + 1).fill(Infinity);
+    const stack = [[1, 0, -1]];
+    while (stack.length) {
+        let [i, c, p] = stack[stack.length - 1];
         if (LR[i][0] === 0 && LR[i][1] === 0) {
-            return 0
+            dp[i] = 0;
+            c = 2
+        }
+        if (c === 2) {
+            stack.pop();
+            if (stack.length === 0) break;
+            const top = stack[stack.length - 1];
+            if (top[1] === 1) {
+                dp[p] = Math.min(dp[i] + (str[p - 1] !== 'L'), dp[p]);
+            } else if (top[1] === 2) {
+                dp[p] = Math.min(dp[i] + (str[p - 1] !== 'R'), dp[p]);
+            }
+            continue
         }
 
-        let res = Infinity
-        if (LR[i][0] !== 0) {
-            res = Math.min(res, dfs(LR[i][0]) + (str[i - 1] === 'L' ? 0 : 1));
+        if (c === 0) {
+            stack[stack.length - 1][1] ++
+            if (LR[i][0] !== 0) stack.push([LR[i][0], 0, i]);
+        } else if (c === 1) {
+            stack[stack.length - 1][1] ++;
+            if (LR[i][1] !== 0) stack.push([LR[i][1], 0, i]);
         }
-        if (LR[i][1] !== 0) {
-            res = Math.min(res, dfs(LR[i][1]) + (str[i - 1] === 'R' ? 0 : 1))
-        }
-
-        return res;
     }
-    cout(dfs(1))
+    cout(dp[1]);
 }
 
 process.stdin.resume();
