5e0087e2adda42ac320eedaad46951d07fa62612
coolCicada <1475395755@qq.com>
2024-03-29 01:03:55 +0800

    feat: 微任务递归写法

--- DIV2/911/C.js
+++ DIV2/911/C.js
@@ -1,68 +1,64 @@
-function main() {
+async function main() {
     let t = cin();
-    while (t --) solve()
+    while (t--) await solve()
 }
 
-function solve() {
+const N = 3e5 + 1
+const L = new Array(N).fill(0)
+const R = new Array(N).fill(0)
+
+async function solve() {
     const n = cin();
     const str = cinS()
     const LR = new Array(n + 1).fill(0);
-    for (let i = 1; i <= n; i ++) {
-        LR[i] = [cin(), cin()]
+    for (let i = 1; i <= n; i++) {
+        L[i] = cin()
+        R[i] = cin()
     }
 
-    const dp = new Array(n + 1).fill(Infinity);
-    const stack = [[1, 0, -1]];
-    while (stack.length) {
-        let [i, c, p] = stack[stack.length - 1];
-        if (LR[i][0] === 0 && LR[i][1] === 0) {
-            dp[i] = 0;
-            c = 2
-        }
-        if (c === 2) {
-            stack.pop();
-            if (stack.length === 0) break;
-            const top = stack[stack.length - 1];
-            if (top[1] === 1) {
-                dp[p] = Math.min(dp[i] + (str[p - 1] !== 'L'), dp[p]);
-            } else if (top[1] === 2) {
-                dp[p] = Math.min(dp[i] + (str[p - 1] !== 'R'), dp[p]);
-            }
-            continue
+    async function dfs(i) {
+        await undefined
+        if (L[i] === 0 && R[i] === 0) {
+            return 0
         }
 
-        if (c === 0) {
-            stack[stack.length - 1][1] ++
-            if (LR[i][0] !== 0) stack.push([LR[i][0], 0, i]);
-        } else if (c === 1) {
-            stack[stack.length - 1][1] ++;
-            if (LR[i][1] !== 0) stack.push([LR[i][1], 0, i]);
+        let res = Infinity
+        if (L[i] !== 0) {
+            res = Math.min(res, await dfs(L[i]) + (str[i - 1] === 'L' ? 0 : 1));
         }
+        if (R[i] !== 0) {
+            res = Math.min(res, await dfs(R[i]) + (str[i - 1] === 'R' ? 0 : 1))
+        }
+
+        return res;
     }
-    cout(dp[1]);
+    cout(await dfs(1))
 }
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
-let inputString = '', content = null, cid = 0;
+let inputString = '',
+    content = null,
+    cid = 0;
 
 process.stdin.on('data', (inputStdin) => {
     inputString += inputStdin;
 });
 
 process.stdin.on('end', (_) => {
-    content=inputString.trim().split(/\s+/), cid=0;
+    content = inputString.trim().split(/\s+/), cid = 0;
     main();
 })
 
 function cin() {
     return Number(content[cid++])
 }
+
 function cinS() {
     return content[cid++]
 }
 
 function cout(...args) {
-    console.log(...args);
+    console.log(args.join(' '));
 }
