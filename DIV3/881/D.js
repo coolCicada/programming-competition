df4a1ef5fcea8967212b79a651f95783301b7c01
coolCicada <1475395755@qq.com>
2024-03-29 01:13:09 +0800

    feat: 异步写法

--- DIV3/881/D.js
+++ DIV3/881/D.js
@@ -1,60 +1,62 @@
-function main() {
+async function main() {
     let t = cin();
-    while (t --) solve()
+    while (t--) await solve()
 }
 
-function solve() {
+async function solve() {
     const n = cin();
     const lines = new Array(n + 1).fill(0).map(() => [])
-    for (let i = 0; i < n - 1; i ++) {
-        const a = cin(), b = cin();
+    for (let i = 0; i < n - 1; i++) {
+        const a = cin(),
+            b = cin();
         lines[a].push(b)
         lines[b].push(a)
     }
+    const cnts = new Array(n + 1).fill(0);
 
-    const countArr = new Array(n + 1).fill(0);
-    const stack = [[1, 0, -1]];
-    while (stack.length) {
-        const [index, i, p] = stack[stack.length - 1];
-        const nb = lines[index];
-        if (i < nb.length) {
-            stack[stack.length - 1][1] ++
-            if (nb[i] !== p) {
-                stack.push([nb[i], 0, index])
+    async function dfs(index, from) {
+        await undefined
+        let isEnd = true;
+        for (let i = 0; i < lines[index].length; i++) {
+            if (lines[index][i] !== from) {
+                isEnd = false
+                cnts[index] += await dfs(lines[index][i], index);
             }
-        } else {
-            stack.pop();
-            if (nb.length === 1 && nb[0] === p) {
-                countArr[index] = 1
-            }
-            countArr[p] += countArr[index]
         }
+        if (isEnd) cnts[index] = 1;
+        return cnts[index]
     }
 
+    await dfs(1, 0);
+
     const q = cin();
-    for (let i = 0; i < q; i ++) {
-        const a = cin(), b = cin();
-        cout(countArr[a] * countArr[b]);
+    for (let i = 0; i < q; i++) {
+        const a = cin(),
+            b = cin();
+        cout(cnts[a] * cnts[b]);
     }
 }
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
-let inputString = '', content = null, cid = 0;
+let inputString = '',
+    content = null,
+    cid = 0;
 
 process.stdin.on('data', (inputStdin) => {
     inputString += inputStdin;
 });
 
 process.stdin.on('end', (_) => {
-    content=inputString.trim().split(/\s+/), cid=0;
+    content = inputString.trim().split(/\s+/), cid = 0;
     main();
 })
 
 function cin() {
     return Number(content[cid++])
 }
+
 function cinS() {
     return content[cid++]
 }
