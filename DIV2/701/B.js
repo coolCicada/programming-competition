8d14921ad1fa763a42f1bd4cef2a8301c9cf5a3e
coolCicada <1475395755@qq.com>
2024-03-29 11:18:15 +0800

    feat: 复习

--- DIV2/701/B.js
+++ DIV2/701/B.js
@@ -1,40 +1,11 @@
 function main() {
     const n = cin(), q = cin(), k = cin();
-    const arr = new Array(n);
-    for (let i = 0; i < n; i ++) {
-        arr[i] = cin();
-    }
-    const dp = new Array(n);
-    for (let i = 0; i < n; i ++) {
-        if (i === 0) {
-            dp[i] = arr[i + 1] - 2
-        } else if (i === n - 1) {
-            dp[i] = k - arr[n - 2] - 1
-        } else {
-            dp[i] = arr[i + 1] - arr[i - 1] - 2
-        }
-    }
-    const preDp = new Array(n + 1).fill(0)
-    for (let i = 1; i <= n; i ++) {
-        preDp[i] = preDp[i - 1] + dp[i - 1];
-    }
-    // cout(preDp)
-    // cout(dp)
+    const arr = new Array(n + 1);
+    for (let i = 1; i <= n; i ++) arr[i] = cin();
+
     for (let i = 0; i < q; i ++) {
         const l = cin(), r = cin();
-        if (l === r) {
-            cout(k - 1)
-            continue
-        }
-        let total = 0
-        total += preDp[r] - preDp[l - 1]
-        if (l !== 1) {
-            total += arr[l - 2]
-        }
-        if (r !== n) {
-            total += k - arr[r] + 1
-        }
-        cout(total)
+        cout(2 * (arr[r] - arr[l] - 1) - 2 * (r - l - 1) + arr[l] - 1 + k - arr[r]);
     }
 }
 
