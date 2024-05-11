d0490b1ec461c7fe793c019cc3a97828ad6873da
梁赛 <1475395755@qq.com>
2024-05-11 17:47:56 +0800

    feat: codechef PERMXORITY

---
+++ codeChef/1400-1500/PERMXORITY.js
@@ -0,0 +1,36 @@
+function main() {
+    let t = cin();
+    while (t --) solve ()
+}
+
+function solve() {
+    const N = cin();
+    if (N === 2) {
+        return cout(-1);
+    }
+    if (N % 2 === 0) {
+        process.stdout.write('2 ')
+        process.stdout.write('3 ')
+        process.stdout.write('1 ')
+
+        for (let i = 4; i <= N; i ++) {
+            process.stdout.write(`${i} `)
+        }
+        cout()
+    } else {
+        for (let i = 1; i <= N; i ++) {
+            process.stdout.write(`${i} `)
+        }
+        cout()
+    }
+
+}
+
+let inputString = '', content = null, cid = 0;
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (inputStdin) => {inputString += inputStdin;});
+process.stdin.on('end', (_) => {content=inputString.trim().split(/\s+/), cid=0;main();})
+function cin(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = Number(content[cid++]); } return arr; } return Number(content[cid++]) }
+function cinS(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = content[cid++]; } return arr; } return content[cid++] }
+function cout(...args) {process.stdout.write(`${args.join(' ')}\n`)}
