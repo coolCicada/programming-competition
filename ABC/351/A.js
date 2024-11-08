9a319467062f2ab671ccb2e9e93905cd64838789
梁赛 <liangsai.0@bytedance.com>
2024-11-08 13:48:50 +0800

    feat: ABC/351

---
+++ ABC/351/A.js
@@ -0,0 +1,18 @@
+function main() {
+    const n = cin(9).reduce((c, p) => {
+        return c + p;
+    });
+    const n2 = cin(8).reduce((c, p) => {
+        return c + p;
+    });
+    cout(n - n2 + 1);
+}
+
+let inputString = '', content = null, cid = 0;
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (inputStdin) => {inputString += inputStdin;});
+process.stdin.on('end', (_) => {content=inputString.trim().split(/\s+/), cid=0;main();})
+function cin(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = Number(content[cid++]); } return arr; } return Number(content[cid++]) }
+function cinS(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = content[cid++]; } return arr; } return content[cid++] }
+function cout(...args) {process.stdout.write(`${args.join(' ')}\n`)}
