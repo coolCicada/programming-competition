6fe3bcccb6a95694e507c7376aa77545f09ed70f
matt <1475395755@qq.com>
2024-12-13 00:24:26 +0800

    feat: codechef CHCOINSG

---
+++ codeChef/1400-1500/CHCOINSG.js
@@ -0,0 +1,22 @@
+function main() {
+    let n = cin()
+    while (n -- ) solve()
+}
+
+function solve() {
+    let n = cin();
+    if (n % 6 === 0) {
+        cout('Misha')
+    } else {
+        cout('Chef')
+    }
+}
+
+let inputString = '', content = null, cid = 0;
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (inputStdin) => {inputString += inputStdin;});
+process.stdin.on('end', (_) => {content=inputString.trim().split(/\s+/), cid=0;main();})
+function cin(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = Number(content[cid++]); } return arr; } return Number(content[cid++]) }
+function cinS(n) { if (n) { const arr = new Array(n); for (let i = 0; i < n; i ++) { arr[i] = content[cid++]; } return arr; } return content[cid++] }
+function cout(...args) {process.stdout.write(`${args.join(' ')}\n`)}
